23f980538e63ba16fcb2ed9faca9c55afc23e8c0
Keval <keval.langalia@live.com>
2022-04-10 21:04:08 -0400

    tabs added in place of home screen

--- App.js
+++ App.js
@@ -1,18 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons,FontAwesome5,AntDesign,Entypo,Fontisto,MaterialIcons} from '@expo/vector-icons';
+import { AuthenticatedUserProvider, AuthenticatedUserContext } from './navigation/AuthenticatedUserProvider';
+
 import LoginScreen from './screens/login/LoginScreen';
 import HomeScreen from './screens/home/HomeScreen';
-import { AuthenticatedUserProvider } from './navigation/AuthenticatedUserProvider';
 import AboutMeScreen from './screens/register/AboutMeScreen';
 import PreferencesScreen from './screens/register/PreferencesScreen';
 import InterestsScreen from './screens/register/InterestsScreen';
 import PhotosScreen from './screens/register/PhotosScreen';
 import SplashScreen from './screens/login/SplashScreen';
+import MatchesScreen from './screens/home/MatchesScreen';
+import AccountScreen from './screens/home/AccountScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = ({navigation}) => {
+  const { user, setUser} = useContext(AuthenticatedUserContext) ;
+  React.useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+
+    });
+      // Return the function to unsubscribe from the event so it gets removed on unmount
+      return unsubscribe;
+    }, [navigation]);
+
+  return (    
+      <Tab.Navigator 
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconColor;
+            let iconName;
+            if (route.name === 'HomeScreen') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'MatchesScreen') {
+              iconName = focused ? 'chatbox-ellipses' : 'chatbox-ellipses-outline';
+            } else if (route.name === 'AccountScreen') {
+              iconName = focused ? 'person' : 'person-outline';
+            }
+            
+            // else if (route.name === 'UsersListScreen') {
+            //   iconName = focused ? 'people-sharp' : 'people-outline';
+            // }else if (route.name === 'StatisticsScreen') {
+            //   iconName = focused ? 'bar-chart' : 'bar-chart-outline';
+            // }else if (route.name === 'AccountScreen' ||  route.name === 'LoginScreen') {
+            //   iconName = focused ? 'person' : 'person-outline';
+            // }
+
+            // console.log(route.name)
+            iconColor = focused ? '#FFC44E' : 'black';
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={iconColor} />;
+          },
+          tabBarActiveTintColor: 'gray',
+          tabBarInactiveTintColor: 'black',
+          
+          headerTintColor: 'white',
+
+          
+        })}>
+
+          <Tab.Screen name="HomeScreen" component={HomeScreen}  options={{headerShown: false, title:'People'}}/>
+          <Tab.Screen name="MatchesScreen" component={MatchesScreen}  options={{ headerShown: false, title : 'Matches'}} />
+          <Tab.Screen name="AccountScreen" component={AccountScreen}  options={{ headerShown: false, title : 'Account'}} />
+
+        </Tab.Navigator>
+  )
+}
+
+
 
 export default function App() {
   return (
@@ -20,14 +81,13 @@ export default function App() {
 
       <NavigationContainer>
         <Stack.Navigator   initialRouteName='Splash'>
-          <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-          
-          <Stack.Screen options={{ headerShown: false }} name="Splash" component={SplashScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
           <Stack.Screen name="AboutMe" component={AboutMeScreen} />
           <Stack.Screen name="Preferences" component={PreferencesScreen} />
           <Stack.Screen name="Interests" component={InterestsScreen} />
           <Stack.Screen name="Photos" component={PhotosScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     </AuthenticatedUserProvider>
---
+++ screens/home/AccountScreen.js
@@ -0,0 +1,55 @@
+import { useNavigation } from '@react-navigation/core'
+import { AuthenticatedUserContext } from '../../navigation/AuthenticatedUserProvider';
+
+import React, {useContext} from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { auth } from '../../firebase/config'
+
+const AccountScreen = ({navigation}) => {
+  const { user, setUser } = useContext(AuthenticatedUserContext);
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        setUser(null)
+        navigation.replace("Login")
+      })
+      .catch(error => alert(error.message))
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Email: {auth.currentUser?.email}</Text>
+      <TouchableOpacity
+        onPress={handleSignOut}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default AccountScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+   button: {
+    backgroundColor: '#0782F9',
+    width: '60%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
