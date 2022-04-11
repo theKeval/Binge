9766597d0bf239e5cc439371bdefedae084e0cbf
eduardocardona93 <eduardoacm93@gmail.com>
2022-04-10 23:10:26 -0400

    image update to server left

--- screens/register/PhotosScreen.js
+++ screens/register/PhotosScreen.js
@@ -1,10 +1,20 @@
 import { StyleSheet, Text, View,TouchableOpacity,Button, Image, Platform,Share,StatusBar, ActivityIndicator} from 'react-native';
 import React, { useState, useEffect } from 'react';
-// import ImagePick from '../../components/imagePick';
+import ImageUploader from '../../components/ImageUploader';
+import useWindowDimensions from 'react-native/Libraries/Utilities/useWindowDimensions';
 
 const PhotosScreen = ({navigation}) => {
-    const [image, imageSet] = useState(null);
-    const [uploading, uploadingSet] = useState(false);
+    const {height, width} = useWindowDimensions();
+
+    const [image1, image1Set] = useState(null);
+    const [image2, image2Set] = useState(null);
+    const [image3, image3Set] = useState(null);
+    const [image4, image4Set] = useState(null);
+    const [image5, image5Set] = useState(null);
+    const [image6, image6Set] = useState(null);
+    const [image7, image7Set] = useState(null);
+    const [image8, image8Set] = useState(null);
+    const [image9, image9Set] = useState(null);
     React.useEffect(() => {
         navigation.setOptions({
             headerRight: () => {
@@ -31,23 +41,67 @@ const PhotosScreen = ({navigation}) => {
     // </View>
     <View  style ={styles.container}>
         <View  style={styles.row}>
-            {/* <ImagePick ></ImagePick> */}
+            <View  style={styles.col}>
+                <ImageUploader imageURL={image1} onSetImageURL={(url)=>{image1Set(url)}} widthImg={width*0.625} heightImg={width*0.625}></ImageUploader>
+            </View>
+            <View  style={styles.col}>
+                <ImageUploader imageURL={image2} onSetImageURL={(url)=>{image2Set(url)}} widthImg={width*0.25} heightImg={width*0.25}></ImageUploader>
+
+                <ImageUploader imageURL={image3} onSetImageURL={(url)=>{image3Set(url)}} widthImg={width*0.25} heightImg={width*0.25}></ImageUploader>
+            </View>
+        </View>
+        <View  style={styles.row}>
+            <View  style={styles.col}>
+                <ImageUploader imageURL={image4} onSetImageURL={(url)=>{image4Set(url)}} widthImg={width*0.25} heightImg={width*0.25}></ImageUploader>
+            </View>
+            <View  style={styles.col}>
+                <ImageUploader imageURL={image5} onSetImageURL={(url)=>{image5Set(url)}} widthImg={width*0.25} heightImg={width*0.25}></ImageUploader>
+            </View>
+            <View  style={styles.col}>
+                <ImageUploader imageURL={image6} onSetImageURL={(url)=>{image6Set(url)}} widthImg={width*0.25} heightImg={width*0.25}></ImageUploader>
+            </View>
+        </View>
+        <View  style={styles.row}>
+            <View  style={styles.col}>
+                <ImageUploader imageURL={image7} onSetImageURL={(url)=>{image7Set(url)}} widthImg={width*0.25} heightImg={width*0.25}></ImageUploader>
+            </View>
+            <View  style={styles.col}>
+                <ImageUploader imageURL={image8} onSetImageURL={(url)=>{image8Set(url)}} widthImg={width*0.25} heightImg={width*0.25}></ImageUploader>
+            </View>
+            <View  style={styles.col}>
+                <ImageUploader imageURL={image9} onSetImageURL={(url)=>{image9Set(url)}} widthImg={width*0.25} heightImg={width*0.25}></ImageUploader>
+            </View>
         </View>
     </View>
   )
 }
-
-export default PhotosScreen
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+
         backgroundColor: '#FFFAEE',
         paddingHorizontal: '5%',
         paddingTop: 10
     },
-    row: {
+    col: {
+        flexDirection: 'column',
         justifyContent: 'center',
-        marginTop: 10,
+        alignContent:'center',
+        padding: 5,
+    },
+    row: {
+        marginVertical: 5,
+
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignContent:'center',
+    },
+    bigImg: {
+        
+    },
+    tinyImg: {
+        
     },
 })
+export default PhotosScreen
+
