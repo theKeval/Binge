e2ca0aef750636b4018fe5cbf6778227ffa4056c
LinoHernandez <82666758+LinoHernandez@users.noreply.github.com>
2022-04-12 17:01:11 -0400

    chat screen

---
+++ screens/home/ChatScreen.js
@@ -0,0 +1,72 @@
+import React, {useState, useEffect, useLayoutEffect, useCallback} from 'react';
+import {TouchableOpacity, Text } from 'react-native';
+import {GiftedChat} from 'react-gifted-chat';
+import {collection, addDoc, orderBy, query, onSnapshot } from 'firebase/firestore';
+import { signOut } from 'firebase/auth';
+import {auth, database } from '../../firebase/config';
+import { useNavigation } from '@react-navigation/native';
+import {AntDesign} from '@expo/vector-icons';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+export default function Chat() {
+    const [messages, setMessages]=useState([]);
+    const navigation = useNavigation();
+
+    const onSignOut = () => {
+        signOut(auth).catch(error => console.log(error));
+    };
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+            <TouchableOpacity style={{marginRight: 10}} onPress={onSignOut}>
+                <AntDesign name="logout" size={24} color='white' style={{marginRight: 10}}/>
+            </TouchableOpacity>
+            )
+        });
+    }, [navigation]);
+
+    useLayoutEffect(() => {
+        const collectionRef = collection(database, 'chats');
+        const q = query(collectionRef, orderBy('createdAt', 'desc'));
+
+        const unsubscribe = onSnapshot(q, snapshot => {
+            console.log('snapshot');
+            setMessages(
+                snapshot.docs.map(doc => ({
+                    _id: doc.id,
+                    createdAt: doc.data().createdAt.toDate(),
+                    text: doc.data().text,
+                    user: doc.data().user
+                }))
+            )
+        });
+        return () => unsubscribe();
+    },  []);
+
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
+
+        const { _id, createdAt, text, user} = messages[0];
+        addDoc(collection(database, 'chats'), {
+            _id,
+            createdAt,
+            text,
+            user
+        });
+    }, []);
+
+    return (
+        <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+                _id: auth?.currentUser?.email,
+            }}
+            messagesContainerStyle={{
+                backgroundColor: 'white'
+            }}
+            
+        />
+    )
+}
