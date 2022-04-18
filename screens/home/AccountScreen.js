11767dd4870062af8d606e097ffe17f09ddaadf4
Keval <keval.langalia@live.com>
2022-04-18 12:59:34 -0400

    bug solved related to error when logging out

--- screens/home/AccountScreen.js
+++ screens/home/AccountScreen.js
@@ -26,11 +26,11 @@ const AccountScreen = ({navigation}) => {
 
   return (
     <View style={styles.container}>
-      <Image source={{ uri: user.profilePicture  }} style={{ width: width*0.625, height: width*0.625 , borderWidth: 10, borderColor: "#009B81", borderRadius: 50}} />
+      <Image source={{ uri: user ? user.profilePicture : 'https://cdn3.vectorstock.com/i/thumb-large/11/72/outline-profil-user-or-avatar-icon-isolated-vector-35681172.jpg' }} style={{ width: width*0.625, height: width*0.625 , borderWidth: 10, borderColor: "#009B81", borderRadius: 50}} />
 
       <View style={[styles.profile]}>
         
-        <Text style={styles.username}>{user.firstName +" "+user.lastName}</Text>
+        <Text style={styles.username}>{ user ? user.firstName +" "+user.lastName : 'User name'}</Text>
       </View>
 
       <View style={tw("flex flex-row justify-between mb-5")}>
